96e7fcc8517907c4ced043ab7588462f1d616e8d
Jason Bert <jason.bert@goblinrockets.com>
2019-04-01 15:29:36 +0100

    Fixed incrementation of payable accounts tracking and added payable actions

--- rewards_pool/src/rewardspool.cpp
+++ rewards_pool/src/rewardspool.cpp
@@ -11,7 +11,7 @@ using std::string;
 namespace enterprise {
    class [[eosio::contract]] rewardspool : public contract {
       public:
-         rewardspool(name receiver, name code, datastream<const char*> ds) : contract(receiver, code, ds), state(_self, _self.value) { }
+         rewardspool(name receiver, name code, datastream<const char*> ds) : contract(receiver, code, ds), current_state(_self, _self.value) { }
 
          [[eosio::action]]
          void addaccaction(name account) {
@@ -19,30 +19,49 @@ namespace enterprise {
             auto existing = act_account.find(account.value);
 
             if (existing == act_account.end()) {
-               const auto& exaa = *existing;
-               act_account.modify(exaa, _self, [&](auto& aa) {
-                  aa.payable_actions = aa.payable_actions++;
-               });
-            }
-            else {
                act_account.emplace(_self, [&](auto& aa) {
                   aa.account = account;
                   aa.payable_actions = 1;
                });
-               auto current_state = state.get();
-
-               current_state.payable_accounts = current_state.payable_accounts++;
-
-               state.set(current_state, _self);
+               increment_payable_accounts();
+               increment_payable_actions();
             }
+            else {
+               const auto& exaa = *existing;
+               act_account.modify(exaa, _self, [&](auto& aa) {
+                  aa.payable_actions = ++aa.payable_actions;
+               });
+               increment_payable_actions();
+            }
+         }
+
+         [[eosio::action]]
+         void setpayables(unsigned long payable_accounts, unsigned long payable_actions) {
+            auto c_state = current_state.get_or_create(_self, state { 0, 0 });;
+
+            c_state.payable_accounts = payable_accounts;
+            c_state.payable_actions = payable_actions;
+
+            current_state.set(c_state, _self);
+         }
+
+         [[eosio::action]]
+         void incpayables() {
+            increment_payable_actions();
+         }
+
+         [[eosio::action]]
+         void clearstate() {
+            current_state.remove();
          }
 
       private:
          struct [[eosio::table]] state {
             unsigned long payable_accounts;
+            unsigned long payable_actions;
          };
          typedef eosio::singleton<"state"_n, state> s_state;
-         s_state state;
+         s_state current_state;
 
          struct [[eosio::table]] action_account {
             name account;
@@ -51,7 +70,23 @@ namespace enterprise {
             uint64_t primary_key() const { return account.value; }
          };
          typedef eosio::multi_index<"actionaccs"_n, action_account> action_accounts;
+
+         void increment_payable_actions() {
+            auto c_state = current_state.get_or_create(_self, state { 0, 0 });
+
+            c_state.payable_actions = c_state.payable_actions + 1;
+
+            current_state.set(c_state, _self);
+         }
+
+         void increment_payable_accounts() {
+            auto c_state = current_state.get_or_create(_self, state { 0, 0 });
+
+            c_state.payable_accounts = c_state.payable_accounts + 1;
+
+            current_state.set(c_state, _self);
+         }
    };
 }
 
-EOSIO_DISPATCH(enterprise::rewardspool, (addaccaction))
+EOSIO_DISPATCH(enterprise::rewardspool, (addaccaction)(setpayables)(incpayables)(clearstate))
