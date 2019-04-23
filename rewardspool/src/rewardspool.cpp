c136064734cd8bd5f3513529cc4db39733405323
Jason Bert <jason.bert@goblinrockets.com>
2019-04-23 18:36:59 +0100

    Added checks to ensure the same UUID is not used

--- rewardspool/src/rewardspool.cpp
+++ rewardspool/src/rewardspool.cpp
@@ -16,11 +16,19 @@ ACTION rewardspool::createact(name source, name owner, uint64_t type_id, string 
   rewards_action_types actions_types(_self, _self.value);
   auto action_type = actions_types.find(type_id);
   check(action_type != actions_types.end(), "Action type does not exist");
-
-  rewards_actions actions(_self, type_id);
   
   auto uuid_raw = source.to_string() + owner.to_string() + std::to_string(type_id) + uuid_salt;
   checksum256 uuid_hash = sha256(const_cast<char*>(uuid_raw.c_str()), uuid_raw.size() * sizeof(char));
+  
+  rewards_actions actions(_self, type_id);
+  auto indexed_by_uuid_actions = actions.template get_index<name("uuid")>();
+  auto existing_action = indexed_by_uuid_actions.find(uuid_hash);
+  check(existing_action == indexed_by_uuid_actions.end(), "Action with UUID already exists");
+  
+  rewards_historical_actions historical_actions(_self, type_id);
+  auto indexed_by_uuid_historical_actions = historical_actions.template get_index<name("uuid")>();
+  auto existing_historical_action = indexed_by_uuid_historical_actions.find(uuid_hash);
+  check(existing_historical_action == indexed_by_uuid_historical_actions.end(), "Hisotrical action with UUID already exists");
   
   actions.emplace(_self, [&](auto& a) {
     a.id = actions.available_primary_key();
