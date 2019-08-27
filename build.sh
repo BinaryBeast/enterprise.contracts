15b5d41d46b0757f4cee5f9fc1cf20cfd073bea2
Jason Bert <jason.bert@goblinrockets.com>
2019-08-27 12:52:18 +0100

    Moved contracts to contracts folder; Moved mock scripts to mocking folder; Changed build script to compile all contracts

--- build.sh
+++ build.sh
@@ -2,28 +2,22 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "Script Directory: $DIR"
-contract=""
+#contract=""
 
-while [ $# -gt 0 ]; do
+# while [ $# -gt 0 ]; do
+#      if [[ $1 == *"--"* ]]; then
+#           echo "$1 | $2"
+#           v="${1/--/}"
+#           declare $v="$2"
+#      fi
+#      shift
+# done
 
-   if [[ $1 == *"--"* ]]; then
-        echo "$1 | $2"
-        v="${1/--/}"
-        declare $v="$2"
-   fi
-
-  shift
+for contractDir in $DIR/contracts/*/
+do
+     cd $contractDir
+     contract=${contractDir%*/}
+     contract=${contract##*/}
+     echo "Building: $contract"
+     eosio-cpp -abigen -I include -L /usr/opt/eosio.cdt/1.6.2/include/eosiolib/contracts/eosio -contract $contract -o $contract.wasm src/$contract.cpp
 done
-
-mkdir -p build
-cd build
-if [[ $contract == "" ]];
-     then
-          for filename in $DIR/src/*.cpp; do
-               echo "Compiling $filename"
-               eosio-cpp $filename -abigen
-          done
-     else
-          echo "Compiling ${contract//\\//}"
-          eosio-cpp $DIR/${contract//\\//} -abigen
-fi
--- mock.sh
+++
@@ -1,6 +0,0 @@
-echo "== EOS MOCK =="
-
-walletName="eosmock"
-
-echo "Creating wallet..."
-cleos wallet create --name $walletName --to-console | tail -1 | awk -F '"' '{pass="$2"}' | echo "Wallet Created | Name: $walletName | PW: $walletPassword"
